6e2c8adcc1b7f6ff2504d252a3767a5910eebc94
Alfred Li <alfred.li@ringcentral.com>
2026-01-23 09:41:33 +0800

    feat: update

---
+++ apps/h5/typings/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_USDT_ADDRESS: string;
+  readonly VITE_USDT_DECIMALS: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
