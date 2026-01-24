388dd58e4b59bfaab2d0ab715bf7f143873ea160
lpeihan <291630202@qq.com>
2026-01-25 01:21:05 +0800

    feat: update

--- apps/h5/src/views/home/Home.vue
+++ apps/h5/src/views/home/Home.vue
@@ -1,18 +1,6 @@
 <template>
   <van-nav-bar :title="$t('tabbar.home')" fixed placeholder />
 
-  <form @submit="handleSubmit">
-    <input v-model="form.name" type="text" />
-
-    <label for="cars">Choose a car:</label>
-    <select id="cars" name="cars">
-      <option value="volvo">Volvo</option>
-      <option value="saab">Saab</option>
-      <option value="opel">Opel</option>
-      <option value="audi">Audi</option>
-    </select>
-  </form>
-
   <div class="py-4 text-center">
     <img src="/logo.svg" class="mx-auto" />
   </div>
@@ -20,15 +8,3 @@
   <Tabbar />
 </template>
 
-<script setup lang="ts">
-import { reactive } from 'vue';
-
-const form = reactive({
-  name: '',
-});
-
-const handleSubmit = (e: Event) => {
-  e.preventDefault();
-  console.log(form.name);
-};
-</script>
