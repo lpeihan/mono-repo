bd8cd7ece7f145f06acfb121ace901fb2387a4a1
Alfred Li <alfred.li@ringcentral.com>
2026-01-26 14:15:14 +0800

    feat: update

--- apps/h5/typings/global.d.ts
+++ apps/h5/typings/global.d.ts
@@ -1,7 +1,15 @@
-export {};
+import type { SvgIconProps } from '@mono/components';
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    SvgIcon: import('vue').DefineComponent<SvgIconProps>;
+  }
+}
 
 declare module 'axios' {
   interface AxiosRequestConfig {
     showError?: boolean;
   }
 }
+
+export {};
